1be1a27294e2282542958341cd904256bc76b278
Soliman <ahmedsliman215@gmail.com>
2021-12-13 17:28:24 +0200

    Digital Clock With STM32F103C8T6

---
+++ src/main.c
@@ -0,0 +1,100 @@
+
+#include "..\include\STD_TYPES.h"
+#include "..\include\BIT_MATH.h"
+#include "..\include\STM32F103C8.h"
+
+#include "..\include\RCC_interface.h"
+#include "..\include\SysTick_interface.h"
+#include "..\include\GPIO_interface.h"
+
+#include "../include/CLCD_interface.h"
+
+u8 Hours_u8Tens = 1;
+u8 Hours_u8Units = 2;
+
+u8 Minuts_u8Tens = 5;
+u8 Minuts_u8Units = 9;
+
+u8 Second_u8Tens = 5;
+u8 Second_u8Units = 9;
+
+u16 Clock_u16Counter = 3599;
+
+int main (void){
+
+	RCC_voidCLKInit();
+	STK_voidInit();
+	RCC_enumPeripheralCLKEnable(  APB2_BUS ,IOPA );
+	RCC_enumPeripheralCLKEnable(  APB2_BUS ,IOPC );
+
+	CLCD_voidInit();
+
+
+	while(1){
+
+		Clear_voidCLCD();
+		CLCD_voidSetPosition(CLCD_ROW_1,CLCD_COL_1);
+
+		CLCD_void_Send_Number(Hours_u8Tens);
+		CLCD_void_Send_Number(Hours_u8Units);
+
+		CLCD_voidSend_Data(':');
+
+		CLCD_void_Send_Number(Minuts_u8Tens);
+		CLCD_void_Send_Number(Minuts_u8Units);
+
+		CLCD_voidSend_Data(':');
+
+		CLCD_void_Send_Number(Second_u8Tens);
+		CLCD_void_Send_Number(Second_u8Units);
+
+		STK_voidSetBusyWait(1000000);
+		Clock_u16Counter++;
+
+		if( Clock_u16Counter == 3600 )
+		{
+			Hours_u8Units++;
+			if( Hours_u8Units == 10)
+			{
+				Hours_u8Tens++;
+				Hours_u8Units = 0;
+			}else if( Hours_u8Units == 3 && Hours_u8Tens == 1 )
+			{
+				Hours_u8Units = 1;
+				Hours_u8Tens = 0;
+			}
+			Clock_u16Counter = 0;
+
+		}
+		if( Clock_u16Counter%60 == 0 )
+		{
+			Minuts_u8Units++;
+			if( (Minuts_u8Tens == 5) && (Minuts_u8Units == 10) )
+			{
+				Minuts_u8Tens = 0;
+				Minuts_u8Units = 0;
+			}else if( Minuts_u8Units == 10)
+			{
+				Minuts_u8Tens++;
+				Minuts_u8Units = 0;
+			}
+
+		}
+
+		Second_u8Units++;
+		if( Second_u8Units == 10 )
+		{
+			Second_u8Tens ++;
+			Second_u8Units = 0;
+		}
+		if ( Second_u8Units == 0 && Second_u8Tens == 6)
+		{
+			Second_u8Tens = 0;
+			Second_u8Units = 0;
+		}
+
+
+	}
+
+	return 0;
+}
